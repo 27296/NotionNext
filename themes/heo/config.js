5ea971d205fad682b1fa5d0d10260ce106b11e46
王俊涛 <79389287+27296@users.noreply.github.com>
2024-08-31 23:09:03 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -7,9 +7,9 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到小王的博客', url: 'https://uaicode.com' }
-  ],
+  // HEO_NOTICE_BAR: [
+  //   { title: '欢迎来到小王的博客', url: 'https://uaicode.com' }
+  // ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
